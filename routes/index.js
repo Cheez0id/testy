736414082e7313ca38b9dae9debc213af84c3da7
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-26 14:40:21 -0500

    jung is do be a genuis

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const app = require('express').Router();
 
+const User = require('../models/User');
+const { request } = require('express');
+const Product = require('../models/Products');
+
 // localhost:6969/api/
 app.get('/', (req, res) => {
-	res.JSON('Hello from this route!!');
+	User.findAll().then((data) => {
+		res.json(data);
+	});
+});
+
+
+app.post('/user', (req, res) => {
+	User.create({
+		name: req.body.name,
+		credit_card: req.body.credit_card,
+		address: req.body.address,
+	}).then((newUser) => {
+		res.json(newUser);
+	});
 });
 
 module.exports = app;
+
+//
